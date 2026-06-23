89da9960496d10dd711c081427e6e53bb655c37a
25024 <c0a25024b7@edu.teu.ac.jp>
2026-06-23 15:59:57 +0900

    演習課題１スコアの実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -140,6 +140,18 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+class Score:
+    def __init__(self):
+        self.fonto = pg.font.Font(None, 50)
+        self.color = (0, 0, 255)
+        self.score = 0
+        self.img = self.fonto.render( f"Score:{self.score}", 0, self.color)
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.center = 100,600
+
+    def update(self, screen: pg.Surface):
+        self.img = self.fonto.render( f"Score:{self.score}", 0, self.color)
+        screen.blit(self.img, self.rct)
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
@@ -153,6 +165,7 @@ def main():
     #     bombs.append(bomb)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     beam = None  # ゲーム初期化時にはビームは存在しない
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -183,6 +196,7 @@ def main():
                     beam = None
                     bomb = None
                     bombs[i] = None
+                    score.score += 1
         
         bombs = [bomb for bomb in bombs if bomb is not None]
 
@@ -192,6 +206,7 @@ def main():
             beam.update(screen)  
         for bomb in bombs:
             bomb.update(screen)
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
